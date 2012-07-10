a14f3625ee739998e0ac21ad16d8791c07b3b9e1
Ivan Sopov <isopov@reveredata.com>
2012-07-10 20:09:10 +0300

    functionality has been implemented - need to fix some bugs and try to refactor code

--- src/main/java/com/sopovs/moradanen/vaadin/ui/MainWindow.java
+++ src/main/java/com/sopovs/moradanen/vaadin/ui/MainWindow.java
@@ -5,11 +5,19 @@
 
 package com.sopovs.moradanen.vaadin.ui;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import com.google.inject.servlet.SessionScoped;
 import com.sopovs.moradanen.vaadin.dao.DummyDao;
+import com.sopovs.moradanen.vaadin.domain.Company;
+import com.sopovs.moradanen.vaadin.domain.Person;
 import com.vaadin.data.Item;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalSplitPanel;
@@ -25,6 +33,10 @@ public class MainWindow extends Window {
 	@Inject
 	private SectorTree sectorTree;
 
+	private Table companyTable;
+
+	private Table personTable;
+
 	public MainWindow() {
 		super("Vaadin Guice application");
 		setSizeFull();
@@ -35,24 +47,72 @@ public class MainWindow extends Window {
 		HorizontalSplitPanel mainSplit = new HorizontalSplitPanel();
 		mainSplit.setSizeFull();
 
+		sectorTree.init();
 		mainSplit.setFirstComponent(sectorTree);
+		sectorTree.addListener(new ItemClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void itemClick(ItemClickEvent event) {
+				companyTable.removeAllItems();
+				List<Company> companies = dao.findCompaniesBySector((String) event.getItemId()); //WTF is this cast?
+				for (Company company : companies) {
+					Item companyItem = companyTable.addItem(company.getId());
+					companyItem.getItemProperty("id").setValue(company.getId());
+					companyItem.getItemProperty("name").setValue(company.getName());
+
+				}
+			}
+		});
 		VerticalSplitPanel rightSplit = new VerticalSplitPanel();
 		mainSplit.setSecondComponent(rightSplit);
 		rightSplit.setSizeFull();
-		rightSplit.setFirstComponent(createDummyTable());
-		rightSplit.setSecondComponent(createDummyTable());
+		companyTable = createCompanyTable();
+		rightSplit.setFirstComponent(companyTable);
+		personTable = createPersonTable();
+		rightSplit.setSecondComponent(personTable);
 		setContent(mainSplit);
 	}
 
-	private static Table createDummyTable() {
+	private static Table createPersonTable() {
 		Table table = new Table();
 		table.setSizeFull();
-		table.addContainerProperty("Thing", String.class, null);
-		for (int i = 0; i < 50; i++) {
-			Item item = table.addItem("Number " + i);
-			item.getItemProperty("Thing").setValue("Number " + i);
-		}
-
+		table.addContainerProperty("id", String.class, null);
+		table.addContainerProperty("firstName", String.class, null);
+		table.addContainerProperty("secondName", String.class, null);
+		table.addContainerProperty("description", String.class, null);
 		return table;
 	}
+
+	private Table createCompanyTable() {
+		final Table table = new Table();
+		table.setSizeFull();
+		table.addContainerProperty("id", String.class, null);
+		table.addContainerProperty("name", String.class, null);
+
+		table.setSelectable(true);
+		table.setImmediate(true);
+		table.addListener(new ValueChangeListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				personTable.removeAllItems();
+				String companyId = (String) companyTable.getValue();
+				if (companyId == null) {
+					return;
+				}
+				List<Person> persons = dao.findPersonsByCompany(companyId);
+				for (Person person : persons) {
+					Item addItem = personTable.addItem(person.getId());
+					addItem.getItemProperty("id").setValue(person.getId());
+					addItem.getItemProperty("firstName").setValue(person.getFirstName());
+					addItem.getItemProperty("secondName").setValue(person.getSecondName());
+					addItem.getItemProperty("description").setValue(person.getDescription());
+				}
+			}
+		});
+		return table;
+	}
+
 }
--- src/main/java/com/sopovs/moradanen/vaadin/ui/SectorTree.java
+++ src/main/java/com/sopovs/moradanen/vaadin/ui/SectorTree.java
@@ -1,9 +1,37 @@
 package com.sopovs.moradanen.vaadin.ui;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import com.google.inject.servlet.SessionScoped;
+import com.sopovs.moradanen.vaadin.dao.DummyDao;
+import com.sopovs.moradanen.vaadin.domain.Sector;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.HierarchicalContainer;
 import com.vaadin.ui.Tree;
 
 @SessionScoped
 public class SectorTree extends Tree {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private DummyDao dao;
+
+	public void init() {
+		List<Sector> sectors = dao.findAllSectors();
+		HierarchicalContainer container = new HierarchicalContainer();
+		container.addContainerProperty("name", String.class, null);
+		for (Sector sector : sectors) {
+			Item sectorItem = container.addItem(sector.getId());
+			sectorItem.getItemProperty("name").setValue(sector.getName());
+			if (sector.getParentId() != null) {
+				container.setParent(sector.getId(), sector.getParentId());
+			}
+		}
+
+		setContainerDataSource(container);
+		setItemCaptionPropertyId("name");
+	}
 
 }
